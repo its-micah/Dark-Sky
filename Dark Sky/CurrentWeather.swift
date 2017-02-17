139e8050a68259f5fbcda01bafe1ca1688983657
its-micah <mlanier86@gmail.com>
2017-02-16 22:27:59 -0600

    add CurrentWeather model

---
+++ Dark Sky/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Dark Sky
+//
+//  Created by Micah Lanier on 2/16/17.
+//  Copyright © 2017 Micah Lanier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct CurrentWeather {
+    let temperature: Double
+    let precipProbability: Double
+    let icon: UIImage
+    let summary: String
+}
